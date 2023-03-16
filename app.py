0563500bdf4d82bc6c949af2ee23cc1cf0eb45c5
a-pellegrino <52744858+a-pellegrino@users.noreply.github.com>
2023-03-16 18:01:03 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
+import base64
+import textwrap
+import xml.etree.ElementTree as ET
+
 
 
 st.set_page_config(page_title="Dashboard Costi", page_icon=":bar_chart:", layout="wide")
@@ -18,6 +22,43 @@ def get_data_from_excel():
     )
 
     return df
+with open('ARC.svg', 'r') as f:
+    svg_data = f.read()
+def render_svg(svg, raggLC, filtro,filtroC):
+
+    """Renders the given svg string."""
+
+    # carica il file SVG come un oggetto ElementTree
+    tree = ET.fromstring(svg)
+    
+    # cerca l'elemento SVG desiderato per ID
+    
+    if raggLC== filtroC:
+        element_id = filtro  # sostituisci "my-rect" con il valore ID desiderato
+        element = tree.find(".//*[@id='{}']".format(element_id))
+
+        if element is not None:
+            element.set("style", "fill: red;fill-opacity:0.3")
+
+    # converte l'ElementTree modificato in una stringa SVG
+        svg_string = ET.tostring(tree, encoding='unicode')
+    else:
+        svg_string = svg
+    b64 = base64.b64encode(svg_string.encode('utf-8')).decode("utf-8")
+    html = r'<img src="data:image/svg+xml;base64,%s" width=500 height=550/>' % b64
+
+
+    
+    
+    
+    st.write(html, unsafe_allow_html=True)
+
+def render_svg_example():
+    svg = svg_data
+    render_svg(svg, raggLC,filtro, filtroC)
+
+
+
 
 df = get_data_from_excel()
 
@@ -65,15 +106,21 @@ st.markdown("##")
 
 # TOP 
 total_sales = int(df_selection["Total"].sum())
-
+filtro= ''.join(df_selection["raggC"].unique())
+filtroC= ''.join(df_selection["raggLC"].unique())
 average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
 
 left_column, middle_column, right_column = st.columns(3)
 with left_column:
     st.subheader("Costo Totale:")
     st.subheader(f"EUR € {total_sales:,}")
-
-
+    #st.subheader(filtro)
+with middle_column:
+    st.subheader("Livello selezionato:")
+    #if agreeRLC:
+    #if __name__ == '__main__':
+    render_svg_example()
+        
 st.markdown("""---""")
 
 # ARTICOLI [BAR CHART]
